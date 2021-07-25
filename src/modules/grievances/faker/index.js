36c10313b0653347b036ed5d111e0c63d9c207ee
Himank Honey Barve <hbarve1592@gmail.com>
2021-07-25 11:17:49 +0530

    WIP: faker file to generate data(work in progress)

---
+++ src/modules/grievances/faker/index.js
@@ -0,0 +1,23 @@
+/** @format */
+
+// These functions will generate fake data for grievances
+const IssueModel = require("../models/issue");
+const IssueTypeModel = require("../models/issueType");
+
+async function generateIssueTypes() {
+  const list = [
+    "Road Safety & Traffic",
+    "Electricity",
+    "Ration Service",
+    "Housing and Development",
+    "Water Delivery",
+    "Sanitation",
+    "Vigilance & Anti-corruption",
+    "Public Safety",
+    "Healthcare",
+  ];
+}
+
+async function generateIssues() {}
+
+// generateIssues();
